ea74a9055d83153357c7ff3dce435eaeb8ac0284
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2024-04-25 17:22:22 +0900

    :gift: feat: kakao Oauth setting

--- ios/EatFluencer/AppDelegate.mm
+++ ios/EatFluencer/AppDelegate.mm
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 
+#import <RNKakaoLogins.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
@@ -26,6 +27,17 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+
+- (BOOL)application:(UIApplication *)app
+     openURL:(NSURL *)url
+     options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+ if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
+    return [RNKakaoLogins handleOpenUrl: url];
+ }
+
+ return NO;
 }
 
 @end
